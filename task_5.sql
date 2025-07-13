7f30e3da9fd60cbe82bd9090e2502804a18812ac
Kalanza <kalanzavictor@gmail.com>
2025-07-13 20:28:27 +0300

    Correct table name casing in customer insertion script

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
--- This script inserts a single row into the Customers table
+-- This script inserts a single row into the customer table
 -- Database name is passed as argument to mysql command
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
